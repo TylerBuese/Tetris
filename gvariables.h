18eb34a40a112fc7c31ed48d30b21696f8ba4a63
Tyler Buese <tylerbuese@gmail.com>
2021-11-09 11:59:52 -0600

    added new file, made progress

---
+++ gvariables.h
@@ -0,0 +1,33 @@
+#include "includes.h"
+
+// I Shape
+Rectangle Ione {10, 10, 10, 10};
+Rectangle Itwo {20, 10, 10, 10};
+Rectangle Ithree {30, 10, 10, 10};
+Rectangle Ifour {40, 10, 10, 10};
+
+Rectangle Lone {10, 10, 10, 10};
+Rectangle Ltwo {20, 10, 10, 10};
+Rectangle Lthree {30, 10, 10, 10};
+Rectangle Lfour {30, 20, 10, 10};
+
+Rectangle Sone {10, 10, 10, 10};
+Rectangle Stwo {20, 10, 10, 10};
+Rectangle Sthree {10, 20, 10, 10};
+Rectangle Sfour {20, 20, 10, 10};
+
+Rectangle Tone {10, 10, 10, 10};
+Rectangle Ttwo {20, 10, 10, 10};
+Rectangle Tthree {30, 10, 10, 10};
+Rectangle Tfour {20, 20, 10, 10};
+
+Rectangle SQone {10, 10, 10, 10};
+Rectangle SQtwo {20, 10, 10, 10};
+Rectangle SQthree {20, 20, 10, 10};
+Rectangle SQfour {30, 20, 10, 10};
+
+bool pressed {false};
+
+int speed {1}; //depends on difficulty
+
+    
