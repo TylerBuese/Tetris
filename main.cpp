e802e6a3202f6ccbb873ff5f2b3e66bf5d04cf95
bzb0y <tylerbuese@gmail.com>
2021-11-10 11:46:27 -0600

    modified

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ int main()
 {
     WindowInit window;
     window.setWindowWidthAndHeight(720, 1000);
-    InitWindow(window.getWindowWidth(), window.getWindowHeight(), "Shitty Tetris");   
+    InitWindow(window.getWindowWidth(), window.getWindowHeight(), "Shitty Tetris");
     Shape shape;
-
-    //int startValue = GetRandomValue(0, 4);
-    int startValue = 0;
+    IShape ishape;
     
+
+    int startValue = GetRandomValue(0, 4);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        
+
         if (IsKeyPressed(KEY_UP))
         {
             startValue++;
@@ -22,7 +22,8 @@ int main()
 
         if (IsKeyPressed(KEY_DOWN))
         {
-            startValue--;;
+            startValue--;
+            ;
         }
 
         if (IsKeyPressed(KEY_ONE))
@@ -31,22 +32,37 @@ int main()
         }
 
 
-        switch (startValue)
-        {
-            case 0: {shape.drawShape(Ione, Itwo, Ithree, Ifour); break;}
-            case 1: {shape.drawShape(Lone, Ltwo, Lthree, Lfour); break;}
-            case 2: {shape.drawShape(Sone, Stwo, Sthree, Sfour); break;}
-            case 3: {shape.drawShape(Tone, Ttwo, Tthree, Tfour); break;}
-            case 4: {shape.drawShape(SQone, SQtwo, SQthree, SQfour); break;}
+            switch (startValue)
+            {
+            case 0:
+            {
+                shape.drawShape(Ione, Itwo, Ithree, Ifour);
+                break;
+            }
+            case 1:
+            {
+                shape.drawShape(Lone, Ltwo, Lthree, Lfour);
+                break;
+            }
+            case 2:
+            {
+                shape.drawShape(Sone, Stwo, Sthree, Sfour);
+                break;
+            }
+            case 3:
+            {
+                shape.drawShape(Tone, Ttwo, Tthree, Tfour);
+                break;
+            }
+            case 4:
+            {
+                shape.drawShape(SQone, SQtwo, SQthree, SQfour);
+                break;
+            }
+            
         }
 
-
-
         DrawText(std::to_string(startValue).c_str(), 100, 100, 20, WHITE);
-
-        
-
-
 
         ClearBackground(BLACK);
         EndDrawing();
--- shapes.h
+++ shapes.h
@@ -25,30 +25,34 @@ class Shape
         int y {0};
 };
 
-class TetrisPiece
+class IShape
 {
     Rectangle Ione {10, 10, 10, 10};
     Rectangle Itwo {20, 10, 10, 10};
     Rectangle Ithree {30, 10, 10, 10};
     Rectangle Ifour {40, 10, 10, 10};
+    Rectangle shape[4] {Ione, Itwo, Ithree, Ifour};
+    
+    
 
-    Rectangle Lone {10, 10, 10, 10};
-    Rectangle Ltwo {20, 10, 10, 10};
-    Rectangle Lthree {30, 10, 10, 10};
-    Rectangle Lfour {30, 20, 10, 10};
 
-    Rectangle Sone {10, 10, 10, 10};
-    Rectangle Stwo {20, 10, 10, 10};
-    Rectangle Sthree {10, 20, 10, 10};
-    Rectangle Sfour {20, 20, 10, 10};
+    // Rectangle Lone {10, 10, 10, 10};
+    // Rectangle Ltwo {20, 10, 10, 10};
+    // Rectangle Lthree {30, 10, 10, 10};
+    // Rectangle Lfour {30, 20, 10, 10};
 
-    Rectangle Tone {10, 10, 10, 10};
-    Rectangle Ttwo {20, 10, 10, 10};
-    Rectangle Tthree {30, 10, 10, 10};
-    Rectangle Tfour {20, 20, 10, 10};
+    // Rectangle Sone {10, 10, 10, 10};
+    // Rectangle Stwo {20, 10, 10, 10};
+    // Rectangle Sthree {10, 20, 10, 10};
+    // Rectangle Sfour {20, 20, 10, 10};
 
-    Rectangle SQone {10, 10, 10, 10};
-    Rectangle SQtwo {20, 10, 10, 10};
-    Rectangle SQthree {20, 20, 10, 10};
-    Rectangle SQfour {30, 20, 10, 10};
+    // Rectangle Tone {10, 10, 10, 10};
+    // Rectangle Ttwo {20, 10, 10, 10};
+    // Rectangle Tthree {30, 10, 10, 10};
+    // Rectangle Tfour {20, 20, 10, 10};
+
+    // Rectangle SQone {10, 10, 10, 10};
+    // Rectangle SQtwo {20, 10, 10, 10};
+    // Rectangle SQthree {20, 20, 10, 10};
+    // Rectangle SQfour {30, 20, 10, 10};
 };
